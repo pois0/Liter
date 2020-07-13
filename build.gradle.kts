a195d7425ea331e58863eb903de4afe168048d17
poispois <dev@pois.jp>
2020-07-14 05:50:22 +0900

    Introduce Bintray plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,30 @@
+/*
+ * Copyright 2020 poispois
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.text.SimpleDateFormat
+import java.util.*
+
 plugins {
     kotlin("jvm") version "1.3.72"
+    id("com.jfrog.bintray") version "1.8.5"
+    `maven-publish`
 }
 
 group = "jp.pois"
-version = "1.0-SNAPSHOT"
+version = System.getenv("LITER_VERSION")
 
 repositories {
     mavenCentral()
@@ -19,5 +40,49 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+bintray {
+    user = "pois"
+    key = System.getenv("BINTRAY_API")
+
+    publish = true
+    `override` = true
+
+    pkg.apply {
+        repo = "KotlinLibs"
+        name = rootProject.name
+        setLicenses("Apache-2.0")
+
+        version.apply {
+            name = project.version.toString().let {
+                if (it[0] == 'v') it.substring(1) else it
+            }
+            released = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").let {
+                it.timeZone = TimeZone.getTimeZone("UTC")
+                it.format(Date())
+            }
+        }
+    }
+}
+
+publishing {
+    publications.withType<MavenPublication> {
+        pom {
+            name.set(rootProject.name)
+            licenses {
+                license {
+                    name.set("The Apache Software License, Version 2.0")
+                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                }
+            }
+            developers {
+                developer {
+                    name.set("pois")
+                    email.set("dev@pois.jp")
+                }
+            }
+        }
     }
 }
